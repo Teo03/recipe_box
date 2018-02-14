f4a49a3040b70def9a16ad201733e86b276c1184
Teodor Bogoeski <teodorbogoeski@gmailcom>
2018-02-14 16:35:57 +0100

    Some improvements

--- src/components/ShowRecipes.js
+++ src/components/ShowRecipes.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import {  Modal, ModalHeader, ModalBody, ModalFooter, Input, Collapse, Button, CardBody, Card } from 'reactstrap';
 
+var timeout = null;
+
 export class ShowRecipes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             collapse: -1,
-            show: false
+            value: ''
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentWillUpdate(nextProps, nextState){
         localStorage.setItem('recipes', JSON.stringify(nextProps.recipes));
     }
 
-    toggle(){
+    handleChange(name, e) {
+        $('.saved').show();
+        clearTimeout(timeout);
         this.setState({
-            show: !this.state.show
+            value: e.target.value
         });
-    }
-    edit(name, inst, del){
-        this.props.recipes.push({title: name, instructions: inst});
-        this.props.recipes.splice(del, 1);
-        this.props.update();
+        timeout = setTimeout(function() {
+            this.props.recipes.push({
+                title: name,
+                instructions: this.state.value
+            });
+            $('.saved').text('Saved!, please refresh!').fadeOut('slow');
+            this.setState({ collapse: -1 });
+        }.bind(this), 1000);
+        $('.saved').text('Saving...');
     }
 
     render() {
@@ -30,15 +39,15 @@ export class ShowRecipes extends React.Component {
             return (
         <div className="recipe-list" key={index}>
         <div className="recipe" id={'recipe-' + index} onClick={() => this.setState({ collapse: index })}>
-        <h1 id="title">{recipeInfo.title}</h1>
+        <h1 id={"title-" + index}>{recipeInfo.title}</h1>
             <Collapse isOpen={this.state.collapse === index}>
                 <Card>
                     <CardBody>
-                        <textarea cols="80" rows="4" id="instructions" defaultValue={recipeInfo.instructions}></textarea>
+                        <textarea cols="80" rows="4" id="instructions" onChange={(e) => this.handleChange(recipeInfo.title, e)} defaultValue={recipeInfo.instructions}></textarea>
                     </CardBody>
                         <div className="col-sm-12 btns">
                         <Button color="secondary" className='col-sm-4' onClick={() => index = -1}>Cancel</Button>
-                        <Button color='success' className='col-sm-4' id="save" onClick={() => this.edit($('#title').text(), $('#instructions').val(), index)}>Save</Button>
+                        <Button color="success" className='col-sm-4 saved'>Saving...</Button>
                         <Button color="danger" className='col-sm-4' onClick={() => this.props.recipes.splice(index, 1)}>Delete</Button>
                         </div>
                 </Card>
